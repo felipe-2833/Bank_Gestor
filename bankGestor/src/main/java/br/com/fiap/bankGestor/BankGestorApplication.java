5209447a03be7c25c1598854914a578f09ac243d
Felipe-2833 <felipe.elucidator@gmail.com>
2025-03-17 08:41:08 -0300

    inicializando projeto

---
+++ bankGestor/src/main/java/br/com/fiap/bankGestor/BankGestorApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.bankGestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankGestorApplication.class, args);
+	}
+
+}
